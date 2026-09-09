a57a151237bb2025e97e8aec530609155d5eb63b
VISLAVATH HATHIRAM <Vislavathhathiram2003@gmail.com>
2026-09-09 22:34:23 +0530

    Update database.py

--- models/database.py
+++ models/database.py
@@ -1,13 +1,22 @@
+import os
 import mysql.connector
-from config import DB_CONFIG
+
+
+DB_CONFIG = {
+    "host": os.environ.get("DB_HOST", "localhost"),
+    "user": os.environ.get("DB_USER", "root"),
+    "password": os.environ.get("DB_PASSWORD", ""),
+    "database": os.environ.get("DB_NAME", "BankingDB"),
+    "port": int(os.environ.get("DB_PORT", "3306"))
+}
+
 
 def get_connection():
 
-    connection = mysql.connector.connect(
+    return mysql.connector.connect(
         host=DB_CONFIG["host"],
         user=DB_CONFIG["user"],
         password=DB_CONFIG["password"],
-        database=DB_CONFIG["database"]
+        database=DB_CONFIG["database"],
+        port=DB_CONFIG["port"]
     )
-
-    return connection
